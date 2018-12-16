a9e4a23629634853ab2d741198e19cc2865a5ef3
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-12-16 09:15:50 -0500

    header footer work

--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 const Footer = () =>{
   return(
-    <footer className='page-footer'>
-      <div className='container'>
-        <p className='text-center'>&copy;2018 <span>CycleFeverTV</span></p>
-      </div>
+    <footer className='page-footer' style={{backgroundColor:'#333',color:'#fff'}}>
+      <ul className='nav justify-content-center'>
+        <li className='nav-link'>&copy;2018 CycleFeverTV</li>
+        <li className='nav-item'>
+          <a href='https://www.facebook.com/CycleFeverTV/' target='_blank' className='nav-link'><i className='fab fa-facebook-f'></i></a>
+        </li>
+        <li className='nav-item'>
+          <a href='https://twitter.com/CycleFeverTV' target='_blank' className='nav-link'><i className='fab fa-twitter'></i></a>
+        </li>
+        <li className='nav-item'>
+          <a href='https://www.instagram.com/cyclefevertv/' target='_blank' className='nav-link'><i className='fab fa-instagram'></i></a>
+        </li>
+        <li className='nav-item'>
+          <a href='https://www.youtube.com/channel/UC5lBCpUunSuN3lCxxTfkunA' className='nav-link' target='_blank'><i className='fab fa-youtube'></i></a>
+        </li>
+      </ul>
     </footer>
   );
 };
